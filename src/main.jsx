0599ff6090457692752a057f02b48183a8dc42bc
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2024-06-28 20:00:10 +1000

    added router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/Login",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/Register",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/Analytics",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/Dashboard",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
