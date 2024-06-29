8d3f10565d42e9c8698f9c4a9a9cd3223958a138
Clarence Muljadi <z5462540@ad.unsw.edu.au>
2024-06-29 19:18:31 +1000

    updated navbar

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -9,14 +10,37 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AnalyticsIcon from "@mui/icons-material/Analytics";
 import LogoutIcon from "@mui/icons-material/Logout";
+import LoginIcon from "@mui/icons-material/Login";
+
 export default function DrawerExample() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
+  };
+
+  const handleLogout = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
   };
 
   const DrawerList = (
@@ -26,32 +50,45 @@ export default function DrawerExample() {
           <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                <PersonOutlineIcon></PersonOutlineIcon>
+                <PersonOutlineIcon />
               </ListItemIcon>
               <ListItemText primary="Profile" />
             </ListItemButton>
           </ListItem>
         </Link>
-        <Link to="/Analytics">
-          <ListItem disablePadding style={{ marginTop: 2 }}>
-            <ListItemButton>
-              <ListItemIcon>
-                <AnalyticsIcon sx={{ color: "#3da9fc" }}></AnalyticsIcon>
-              </ListItemIcon>
-              <ListItemText primary="Analytics" />
-            </ListItemButton>
-          </ListItem>
-        </Link>
-        <Link to="/Logout">
-          <ListItem disablePadding style={{ marginTop: 2 }}>
-            <ListItemButton>
-              <ListItemIcon>
-                <LogoutIcon />
-              </ListItemIcon>
-              <ListItemText primary="Logout" />
-            </ListItemButton>
-          </ListItem>
-        </Link>
+        {user ? (
+          <>
+            <Link to="/Analytics">
+              <ListItem disablePadding style={{ marginTop: 2 }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AnalyticsIcon sx={{ color: "#3da9fc" }} />
+                  </ListItemIcon>
+                  <ListItemText primary="Analytics" />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <ListItem disablePadding style={{ marginTop: 2 }}>
+              <ListItemButton onClick={handleLogout}>
+                <ListItemIcon>
+                  <LogoutIcon />
+                </ListItemIcon>
+                <ListItemText primary="Logout" />
+              </ListItemButton>
+            </ListItem>
+          </>
+        ) : (
+          <Link to="/login">
+            <ListItem disablePadding style={{ marginTop: 2 }}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LoginIcon />
+                </ListItemIcon>
+                <ListItemText primary="Login" />
+              </ListItemButton>
+            </ListItem>
+          </Link>
+        )}
       </List>
     </Box>
   );
@@ -65,7 +102,7 @@ export default function DrawerExample() {
           fontFamily: "inter, system-ui, Avenir, Helvetica, Arial, sans-serif;",
         }}
       >
-        <MenuIcon className="mr-6" sx={{ color: "#ef4565" }}></MenuIcon>
+        <MenuIcon className="mr-6" sx={{ color: "#ef4565" }} />
         <span className="hidden sm:inline text-lg">Menu</span>
       </Button>
       <Drawer open={open} onClose={toggleDrawer(false)}>
