eac2eaf84bcf34e0775287e75312876fc98d8579
Michael <natawidjajamichael@gmail.com>
2024-06-29 16:48:27 +1000

    add navbar for register and login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,6 +3,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 import Checkbox from '@mui/material/Checkbox';
+import Navbar from '../components/Navbar';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -23,20 +24,24 @@ const textFieldStyle = {
   }
 };
 
+// h-1/3 w-1/3 max-w-screen-sm min-w-fit min-w-[700px]
 const Register = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-slate-100">
-      <div className='flex flex-col h-5/6 w-1/3 max-w-screen-sm min-w-fit bg-bground px-12 py-16 gap-3 rounded-3xl shadow-2xl'>
-        <div className='mb-8'>
-          <h1 className='text-6xl font-bold mt-0 mb-4'>Login</h1>
+    <div className='min-h-screen flex flex-col'>
+        <Navbar/>
+        <div className="min-h-screen flex justify-center items-center bg-slate-100">
+        <div className='flex flex-col  min-h-fit h-[40vh] min-w-[700px] w-screen-md max-w-[850px] bg-bground px-12 py-16 gap-3 rounded-3xl shadow-2xl'>
+            <div className='mb-8'>
+            <h1 className='text-6xl font-bold mt-0 mb-4'>Login</h1>
+            </div>
+            <div className=' flex flex-col items-center justify-start gap-10'>
+                <TextField sx={textFieldStyle} id="email" label="Email Address" variant="outlined"/>
+                <TextField sx={textFieldStyle} id="password" label="Password" variant="outlined"/>
+                <Button sx={{width: "100%", padding: "22px", borderRadius: "12px", background:'#3da9fc'}}variant="contained"><p className='normal-case font-bold text-2xl'>Sign in</p></Button>
+            </div>
+            <p className='text-xl'> <b className='text-button-color hover:text-blue-900'>Forgot Password?</b></p>
         </div>
-        <div className=' flex flex-col items-center justify-start gap-10'>
-            <TextField sx={textFieldStyle} id="email" label="Email Address" variant="outlined"/>
-            <TextField sx={textFieldStyle} id="password" label="Password" variant="outlined"/>
-            <Button sx={{width: "100%", padding: "22px", borderRadius: "12px", background:'#3da9fc'}}variant="contained"><p className='normal-case font-bold text-2xl'>Sign in</p></Button>
         </div>
-        <p className='text-xl'> <b className='text-button-color hover:text-blue-900'>Forgot Password?</b></p>
-      </div>
     </div>
   )
 }
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -3,6 +3,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 import Checkbox from '@mui/material/Checkbox';
+import Navbar from '../components/Navbar';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -25,23 +26,26 @@ const textFieldStyle = {
 
 const Register = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-slate-100">
-      <div className='flex flex-col h-fit w-1/3 max-w-screen-sm min-w-fit bg-bground px-12 py-16 gap-3 rounded-3xl shadow-2xl'>
-        <div className='mb-8'>
-          <h1 className='text-6xl font-bold mb-2'>Sign Up</h1>
+    <div className='min-h-screen flex flex-col'>
+      <Navbar/>
+      <div className="min-h-screen flex justify-center items-center bg-slate-100">
+        <div className='flex flex-col min-h-fit min-w-[700px] w-screen-md max-w-[850px] bg-bground px-12 py-16 gap-3 rounded-3xl shadow-2xl'>
+          <div className='mb-8'>
+            <h1 className='text-6xl font-bold mb-2'>Sign Up</h1>
+          </div>
+          <div className=' flex flex-col items-center justify-start gap-8'>
+            <TextField sx={textFieldStyle} id="name" label="Full Name" variant="outlined"/>
+            <TextField sx={textFieldStyle} id="email" label="Email Address" variant="outlined"/>
+            <TextField sx={textFieldStyle} id="password" label="Password" variant="outlined"/>
+            <TextField sx={textFieldStyle} id="email" label="Confirm Password" variant="outlined"/>
+          </div>
+          <div className='flex items-center'>
+            <Checkbox {...label} sx={{color:'#3da9fc', padding: '2px', '&.Mui-checked': {color: '#3da9fc',},}}/>
+            <p className='m-2 text-xl'>I accept the <b>Terms and Conditions</b></p>
+          </div>
+          <Button sx={{width: "100%", padding: "22px", borderRadius: "12px", background:'#3da9fc'}}variant="contained"><p className='normal-case font-bold text-2xl'>Create an account</p></Button>
+          <p className='text-xl'>Already have an account? <b className='text-button-color hover:text-blue-900'>Login here</b></p>
         </div>
-        <div className=' flex flex-col items-center justify-start gap-8'>
-          <TextField sx={textFieldStyle} id="name" label="Full Name" variant="outlined"/>
-          <TextField sx={textFieldStyle} id="email" label="Email Address" variant="outlined"/>
-          <TextField sx={textFieldStyle} id="password" label="Password" variant="outlined"/>
-          <TextField sx={textFieldStyle} id="email" label="Confirm Password" variant="outlined"/>
-        </div>
-        <div className='flex items-center'>
-          <Checkbox {...label} sx={{color:'#3da9fc', padding: '2px', '&.Mui-checked': {color: '#3da9fc',},}}/>
-          <p className='m-2 text-xl'>I accept the <b>Terms and Conditions</b></p>
-        </div>
-        <Button sx={{width: "100%", padding: "22px", borderRadius: "12px", background:'#3da9fc'}}variant="contained"><p className='normal-case font-bold text-2xl'>Create an account</p></Button>
-        <p className='text-xl'>Already have an account? <b className='text-button-color hover:text-blue-900'>Login here</b></p>
       </div>
     </div>
   )
